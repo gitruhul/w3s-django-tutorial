67556ad16d5f1522f05816c202ba4a4d33200c01
Ruhulla Sheik <ruhulla.sheik@amsoftware.com>
2023-11-09 12:25:30 +0530

    W3S: Display Data: Django Add Test View
    # https://www.w3schools.com/django/django_test_view.php

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -21,4 +21,10 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
-    
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
