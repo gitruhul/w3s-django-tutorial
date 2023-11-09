59d709b2683fdd49c87bd0bce956b89e89b2fa9c
Ruhulla Sheik <ruhulla.sheik@amsoftware.com>
2023-11-09 12:42:40 +0530

    W3S: Django Syntax: Django Template Variables
    # https://www.w3schools.com/django/django_template_variables.php

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -23,8 +23,9 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
