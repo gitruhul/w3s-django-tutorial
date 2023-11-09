04c300222bd8bc452996fdbf64f32d4853b5d922
Ruhulla Sheik <ruhulla.sheik@amsoftware.com>
2023-11-09 12:35:15 +0530

    W3S: Admin: Django Admin - Set Fields to Display
    # https://www.w3schools.com/django/django_admin_set_list_display.php

--- my_tennis_club/members/admin.py
+++ my_tennis_club/members/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Member
 
 # Register your models here.
-admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  
+admin.site.register(Member, MemberAdmin)
