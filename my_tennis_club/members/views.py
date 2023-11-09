3ceb729291d575748783e55ee0bd374a902a180c
Ruhulla Sheik <ruhulla.sheik@amsoftware.com>
2023-11-09 12:52:14 +0530

    W3S: Django Syntax: Django QuerySet - Filter
    # https://www.w3schools.com/django/django_queryset_filter.php

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
   mymembers = Member.objects.all().values()
@@ -23,7 +24,7 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
-  mydata = Member.objects.values_list('firstname')
+  mydata = Member.objects.filter(Q(firstname='Rajesh') | Q(firstname='Amit')).values()
   template = loader.get_template('template.html')
   context = {
     'mymembers': mydata,
